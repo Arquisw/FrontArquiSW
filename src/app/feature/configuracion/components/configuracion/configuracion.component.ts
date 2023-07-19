4f768b8fc2e36623644262efd0e51f6e3e93f8dd
ESTEBAN\Esteban <estebancoloradogonzalez@gmail.com>
2023-07-18 20:46:46 -0500

    se organiza el filtro del menu de navegación

--- src/app/feature/configuracion/components/configuracion/configuracion.component.ts
+++ src/app/feature/configuracion/components/configuracion/configuracion.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PersonaResumen } from '../../shared/model/persona-resumen.model';
+import { ConfiguracionService } from '../../shared/service/configuracion.service';
 
 @Component({
   selector: 'app-configuracion',
@@ -8,10 +10,29 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ConfiguracionComponent implements OnInit {
   usuarioId = 0;
+  personaResumen: PersonaResumen;
+  tieneAsociacion = false;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private configuracionService: ConfiguracionService) {}
 
   ngOnInit(): void {
     this.usuarioId = parseInt(this.route.snapshot.paramMap.get('id'), 10);
+
+    this.consultarUsuario();
+  }
+
+  consultarUsuario(): void {
+    this.configuracionService.consultarUsuarioPorId(this.usuarioId).subscribe((response) => {
+      this.personaResumen = response;
+      this.filtrarMenu(response.roles);
+    });
+  }
+
+  filtrarMenu(roles): void {
+    roles.forEach(rol => {
+      if (rol.nombre === 'ROLE_ASOCIACION') {
+        this.tieneAsociacion = true;
+      }
+    });
   }
 }
--- src/app/feature/configuracion/shared/model/rol.model.ts
+++ src/app/feature/configuracion/shared/model/rol.model.ts
@@ -1,7 +1,15 @@
 export class Rol {
   public nombre: string;
+  public leer: boolean;
+  public escribir: boolean;
+  public actualizar: boolean;
+  public eliminar: boolean;
 
-  constructor(nombre?: string) {
+  constructor(nombre?: string, leer?: boolean, escribir?: boolean, actualizar?: boolean, eliminar?: boolean) {
     this.nombre = nombre || '';
+    this.leer = leer || false;
+    this.escribir = escribir || false;
+    this.actualizar = actualizar || false;
+    this.eliminar = eliminar || false;
   }
 }
