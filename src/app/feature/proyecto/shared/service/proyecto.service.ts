9b89cda585dd0db5998e3057de9c5d2c16354ba3
ESTEBAN\Esteban <estebancoloradogonzalez@gmail.com>
2023-08-07 20:01:28 -0500

    Se comienza a construir el componente del proyecto

--- src/app/feature/proyecto/shared/service/proyecto.service.ts
+++ src/app/feature/proyecto/shared/service/proyecto.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpService } from '@core/services/http.service';
+import { Observable } from 'rxjs';
+import { NecesidadResumen } from 'src/app/feature/proyectos/shared/model/necesidad-resumen.model';
+import { SeleccionResumen } from 'src/app/feature/proyectos/shared/model/seleccion-resumen.model';
+import { environment } from 'src/environments/environment';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ProyectoService {
+  private readonly NECESIDADES_ENDPOINT: string = '/necesidades';
+  private readonly POSTULACIONES_ENDPOINT: string = '/postulaciones';
+  private readonly SELECCIONES_ENDPOINT: string = '/selecciones';
 
-  constructor() { }
+  constructor(private httpService: HttpService) { }
+
+  public consultarNecesidadPorId(id: number): Observable<NecesidadResumen>
+  {
+    return this.httpService.doGetById<NecesidadResumen>(`${environment.endpoint}${this.NECESIDADES_ENDPOINT}/`, id);
+  }
+  public consultarSeleccionesPorProyectoId(id: number): Observable<SeleccionResumen[]>
+  {
+    return this.httpService.doGetById<SeleccionResumen[]>(`${environment.endpoint}${this.POSTULACIONES_ENDPOINT}${this.SELECCIONES_ENDPOINT}/`, id);
+  }
 }
