27f960c3451413048255750d8791fe9d1ed9120a
juanvalenciaceiba <107130614+juanvalenciaceiba@users.noreply.github.com>
2023-08-16 13:06:40 -0500

    se agrega tabla para el compoenete de contrataciones

--- src/app/feature/panel-administrador/components/contrataciones/contrataciones.component.ts
+++ src/app/feature/panel-administrador/components/contrataciones/contrataciones.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+import { AdministradorService } from '../../shared/service/administrador.service';
 
 @Component({
   selector: 'app-contrataciones',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./contrataciones.component.scss']
 })
 export class ContratacionesComponent {
+  necesidadAprobadas: any[] = [];
+  mensajeError= '';
+
+  constructor(private router: Router, private admistradorService: AdministradorService)  { }
+
+  consultaAprobaciones(): void {
+    this.necesidadAprobadas= [];
+    let petecionAprobar;
+    this.admistradorService.consultarNecesidadesPendienteAprobacion().subscribe((response) => {
+      petecionAprobar = response;
+      this.necesidadAprobadas = petecionAprobar;
+      console.log(this.necesidadAprobadas);
+    },
+    (error) => {
+      this.mensajeError=error.message;
+    });
+  }
+
+  abrirPerfilProyecto(id): void {
+    const navigationExtras: NavigationExtras = {
+      state: {
+        id: id,
+        proyecto: false,
+      }
+    };
+    this.router.navigate(['/proyecto'], navigationExtras);
+  }
 
 }
