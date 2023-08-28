ae4babde557e19ec7ef8cae094548fd2088cae97
ESTEBAN\Esteban <estebancoloradogonzalez@gmail.com>
2023-08-27 23:08:54 -0500

    Se crea primera capa de ingenieria de requisitos

--- src/app/feature/ingenieria-de-requisitos/components/ingenieria-de-requisitos/ingenieria-de-requisitos.component.ts
+++ src/app/feature/ingenieria-de-requisitos/components/ingenieria-de-requisitos/ingenieria-de-requisitos.component.ts
@@ -1,5 +1,7 @@
 import { ViewportScroller } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { IngenieriaDeRequisitosService } from '../../shared/service/ingenieria-de-requisitos.service';
+import { ProyectoResumen } from 'src/app/feature/proyectos/shared/model/proyecto-resumen.model';
 
 @Component({
   selector: 'app-ingenieria-de-requisitos',
@@ -8,13 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IngenieriaDeRequisitosComponent implements OnInit {
   proyectoId = 0;
+  proyectoResumen: ProyectoResumen;
+  inicioElProcesoDeConsultoria = false;
 
-  constructor(private viewportScroller: ViewportScroller) { }
+  constructor(private viewportScroller: ViewportScroller, private ingenieriaDeRequisitosService: IngenieriaDeRequisitosService) { }
 
   ngOnInit(): void {
+    this.posicionarPaginaAlInicio();
+
     const params = history.state;
     this.proyectoId = params.id;
 
+    this.consultarProyectoPorId(this.proyectoId);
+
+    console.log(this.inicioElProcesoDeConsultoria);
+  }
+
+  posicionarPaginaAlInicio(): void {
     this.viewportScroller.scrollToPosition([0, 0]);
   }
+
+  consultarProyectoPorId(id: number): void {
+    this.ingenieriaDeRequisitosService.consultarProyectoPorId(id).subscribe((response) => {
+      this.proyectoResumen = response;
+
+      this.evaluarSiInicioElProcesoDeConsultoria();
+    });
+  }
+
+  evaluarSiInicioElProcesoDeConsultoria(): void {
+    if (this.proyectoResumen.aprobacionProyecto.ingenieria &&
+      this.proyectoResumen.aprobacionProyecto.liderDeEquipo &&
+      this.proyectoResumen.aprobacionProyecto.directorDeProyecto) {
+      this.inicioElProcesoDeConsultoria = true;
+    }
+  }
+
+
 }
--- src/app/feature/ingenieria-de-requisitos/shared/service/ingenieria-de-requisitos.service.ts
+++ src/app/feature/ingenieria-de-requisitos/shared/service/ingenieria-de-requisitos.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpService } from '@core/services/http.service';
+import { Observable } from 'rxjs';
+import { ProyectoResumen } from 'src/app/feature/proyectos/shared/model/proyecto-resumen.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 
 export class IngenieriaDeRequisitosService {
+  private readonly NECESIDADES_ENDPOINT: string = '/necesidades';
+  private readonly PROYECTOS_ENDPOINT: string = '/proyectos';
 
-  constructor() { }
+  constructor(private httpService: HttpService) { }
+
+  public consultarProyectoPorId(id: number): Observable<ProyectoResumen>
+  {
+    return this.httpService.doGetById<ProyectoResumen>(`${environment.endpoint}${this.NECESIDADES_ENDPOINT}${this.PROYECTOS_ENDPOINT}/`, id);
+  }
 }
