3c2c320249116c0d87ac2fa9c54646bff7801bd7
CEIBA\esteban.colorado <estecogn@suramericana.com.co>
2023-10-12 15:53:44 -0500

    Se arregla warning de libreria pdf-make

--- src/app/shared/service/pdf-make/pdf-make.service.ts
+++ src/app/shared/service/pdf-make/pdf-make.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfMake from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
 import { RequisitoResumen } from '../../../feature/ingenieria-de-requisitos/shared/model/requisito-resumen.module';
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
